9c81c2ddf3c807e4a1b65b1a79f09ba36f904e40
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-08 22:29:40 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var num1 = 24;
+var num2 = 6;
+console.log(num1 / num2);
+var num3 = 16;
+var num4 = 8;
+var result = (num3 / num4);
+console.log(result);
+var n1 = 2;
+console.log(Math.pow(n1, n1));
+var n2 = 2;
+var n3 = 2;
+console.log(Math.pow(n2, n3));
+var n4 = 10;
+var n5 = 3;
+console.log(n4 % n5);
+var n6 = 16;
+var n7 = 2;
+console.log(n6 % n7);
---
+++ main.ts
@@ -0,0 +1,23 @@
+let num1 = 24;
+let num2 = 6;
+console.log(num1 / num2);
+
+let num3 = 16;
+let num4 = 8;
+let result = (num3 / num4);
+console.log(result);
+
+let n1 = 2;
+console.log(n1 ** n1);
+
+let n2 = 2;
+let n3 = 2;
+console.log(n2 ** n3);
+
+let n4 = 10;
+let n5 = 3;
+console.log(n4 % n5);
+
+let n6 = 16;
+let n7 = 2;
+console.log(n6 % n7);
